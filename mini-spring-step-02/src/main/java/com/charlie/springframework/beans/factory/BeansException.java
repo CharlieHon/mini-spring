aa29fa0da69c7b38a1debc47018ff2e09f03c58c
charlie <2295509496@qq.com>
2024-12-20 20:04:54 +0800

    自定义异常

---
+++ mini-spring-step-02/src/main/java/com/charlie/springframework/beans/factory/BeansException.java
@@ -0,0 +1,16 @@
+package com.charlie.springframework.beans.factory;
+
+/**
+ * @Author: charlie
+ * @CreateTime: Created in 2024/12/20 19:06
+ * @Description: BeansException
+ */
+public class BeansException extends RuntimeException{
+    public BeansException(String msg) {
+        super(msg);
+    }
+
+    public BeansException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
